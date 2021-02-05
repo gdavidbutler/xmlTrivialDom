d04675bfe77d1b8c6ad413d7e67dd43d6d0864d0
G. David Butler <gdavidbutler@users.noreply.github.com>
2021-02-05 01:07:21 +0000

    serialize from item not parent

--- test/main.c
+++ test/main.c
@@ -27,6 +27,7 @@ main(
 ){
   static const char *dn = ":memory:";
   sqlite3 *db;
+  sqlite3_stmt *st;
   unsigned char *bf;
   int rc;
   int i;
@@ -70,14 +71,23 @@ main(
     return (rc);
   }
   sqlite3_free(bf);
-  if (!(bf = (unsigned char *)xql2xml(db, 0, (unsigned int *)&i))) {
-    fprintf(stderr, "xql2xml:%s\n", sqlite3_errmsg(db));
-    return (-1);
+  if ((rc = -sqlite3_prepare_v2(db
+   ,"SELECT \"i\" FROM \"XqlE\" WHERE \"e\"=0 ORDER BY \"o\" ASC"
+   ,-1, &st, 0))) {
+    fprintf(stderr, "sqlite3_prepare_v2:%d:%s\n", rc, sqlite3_errmsg(db));
+    return (rc);
   }
-  fwrite(bf, 1, i, stdout);
-  if (!i || *(bf + i - 1) != '\n')
-    putchar('\n');
-  sqlite3_free(bf);
+  while (sqlite3_step(st) == SQLITE_ROW) {
+    if (!(bf = (unsigned char *)xql2xml(db, sqlite3_column_int64(st, 0), (unsigned int *)&i))) {
+      fprintf(stderr, "xql2xml:%s\n", sqlite3_errmsg(db));
+      return (-1);
+    }
+    fwrite(bf, 1, i, stdout);
+    if (!i || *(bf + i - 1) != '\n')
+      putchar('\n');
+    sqlite3_free(bf);
+  }
+  sqlite3_finalize(st);
   sqlite3_close(db);
   return (0);
 }
